24f0034c21f6265c45bb98da344b2a5ec7810d9b
tommymac2709 <tom.mckenzie@outlook.com>
2024-09-02 22:07:51 +1000

    Refactored GetStat
    Created Dictionary lookup tables that are built once, as opposed to 2x foreach loops running every frame

--- Assets/Shared/Scripts/Stats/Progression.cs
+++ Assets/Shared/Scripts/Stats/Progression.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Progression", menuName = "New Progression", order = 0)]
@@ -5,25 +7,40 @@ public class Progression : ScriptableObject
 {
     [SerializeField] ProgressionCharacterClass[] characterClasses = null;
 
-
+    Dictionary<CharacterClass, Dictionary<Stat, float[]>> lookupTable = null;
 
     public float GetStat(Stat stat, CharacterClass characterClass, int level) 
     {
+        BuildLookup();
+
+        float[] levels = lookupTable[characterClass][stat];
+
+        if (levels.Length < level)
+        {
+            return 0;
+        }
+
+        return levels[level - 1];
+    }
+
+    private void BuildLookup()
+    {
+        if (lookupTable != null) { return; }
+
+        lookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
         foreach (ProgressionCharacterClass progressionClass in characterClasses)
         {
-            if (progressionClass.characterClass != characterClass) continue;
+            var statLookupTable = new Dictionary<Stat, float[]>();  
 
             foreach (ProgressionStat progressionStat in progressionClass.stats)
             {
-                if (progressionStat.stat != stat) continue;
-
-                if (progressionStat.levels.Length < level) continue;
-
-                return progressionStat.levels[level - 1];
-
+                statLookupTable[progressionStat.stat] = progressionStat.levels;
             }
+
+            lookupTable[progressionClass.characterClass] = statLookupTable;
         }
-        return 0;
+
     }
 
     [System.Serializable]
