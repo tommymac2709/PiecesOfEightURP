eb025ccf72efb7f6470502fe9124a73aaca080bd
tommymac2709 <tom.mckenzie@outlook.com>
2024-09-06 11:24:12 +1000

    Mock UI Created

---
+++ Assets/Shared/InventorySystem/UI/Scripts/DisplayInventory.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DisplayInventory : MonoBehaviour
+{
+    public InventorySO inventory;
+    Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
+
+    private void Start()
+    {
+        CreateDisplay();
+    }
+
+    private void Update()
+    {
+        UpdateDisplay();
+    }
+
+    private void CreateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+            obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            itemsDisplayed.Add(inventory.Container[i], obj);
+        }
+    }
+
+    public void UpdateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            if (itemsDisplayed.ContainsKey(inventory.Container[i]))
+            {
+                itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            }
+            else
+            {
+                CreateDisplay();
+            }
+        }
+    }
+}
